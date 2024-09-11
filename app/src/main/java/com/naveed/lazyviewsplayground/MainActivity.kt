1f17e869e7f00fd01a937fe97ad4d468a53142fa
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-09-11 11:16:35 +0500

    add lazyDemo Before  update

--- app/src/main/java/com/naveed/lazyviewsplayground/MainActivity.kt
+++ app/src/main/java/com/naveed/lazyviewsplayground/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.naveed.lazyviewsplayground
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.tooling.preview.Preview
 import com.naveed.lazyviewsplayground.ui.theme.LazyViewsPlaygroundTheme
 
@@ -20,28 +27,38 @@ class MainActivity : ComponentActivity() {
         setContent {
             LazyViewsPlaygroundTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    LazyColumnDemo(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun LazyColumnDemo(modifier: Modifier = Modifier) {
+    Box(
         modifier = modifier
-    )
-}
+            .fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable {
+                    Log.d("LazyColumnDemo", "Box under LazyColumn clicked")
+                },
+        )
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .pointerInteropFilter {
+                    return@pointerInteropFilter false
+                }
+        ) {
+            items(listOf(1, 2, 3)) {
+                Text(text = "Item $it")
+            }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LazyViewsPlaygroundTheme {
-        Greeting("Android")
+        }
     }
 }
